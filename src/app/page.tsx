4b5e07a2ac6f3ea20864ce64e05886aa07e2d58b
Jetnarong Khampeng <jetnarongkompang@gmail.com>
2024-05-13 17:20:02 +0700

    update action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,117 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+
+import { useState } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [winnerPrize, setWinnerPrize] = useState<any>([{ id: 0, text: '', path: '' }])
+
+  const prize = [
+    { id: 1, prize: "You got vacuum cleaner", text: 'Vacuum cleaner🧹', path: '/cleaner.svg' },
+    { id: 2, prize: "You got",text: 'Money 💸', path: '/money.svg' },
+    { id: 3, prize: "Thank you for joining in the fun",text: 'Thank you 😱 ', path: '/thanks.png' },
+    { id: 4, prize: "Thank you for joining in the fun",text: 'Thankyou 🫣', path: '/thanks.png' },
+    { id: 5, prize: "You got apple watch",text: 'Apple Watch ⌚️', path: '/apple.svg' },
+    { id: 6, prize: "Thank you for joining in the fun",text: 'Thank you 😋', path: '/thanks.png' },
+    { id: 7, prize: "Thank you for joining in the fun",text: 'Thank you 🙏', path: '/thanks.png' },
+  ]
+
+  function onRandomPrize() {
+    var item = prize[Math.floor(Math.random() * prize.length)];
+    if(item.id == 2){
+      let getMoney = Math.floor(Math.random() * 9999999);
+      
+      item.prize = item.prize+" "+numberWithCommas(getMoney)+" baht"
+    }
+    
+    setWinnerPrize(item)
+    setShow(true)
+
+  }
+
+  function numberWithCommas(x: any) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Modal
+        size="sm"
+        centered
+        backdrop="static"
+        keyboard={false}
+        show={show}
+        onHide={handleClose}
+        contentClassName="model-prize"
+      >
+        <Modal.Body>
+          <Row>
+            <Col sm={12} className="mt-5">
+              <img src={winnerPrize.path} width={150} alt="" />
+            </Col>
+            <Col sm={12} className="mt-2">
+              Congratulations !
+            </Col>
+            <Col sm={12}>
+              {winnerPrize.prize}
+            </Col>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          </Row>
+          <Button variant="dark" className="mt-3" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Body>
+      </Modal>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+      <Container>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Row style={{ alignContent: 'center' }}>
+          <Col sm={12}>
+            <p className="title-game">Which reward you will get?</p>
+          </Col>
+          <Col sm={12}>
+            <div className="d-flex justify-content-center">
+              {/* <div id="wrapper"> */}
+
+              <div className="wheel">
+                <div className="inner-wheel">
+                  {prize.map((item) =>
+                    <div className="sec"><span className={`fa text-${item.id}`} key={item.id}>{item.text}</span></div>
+                  )}
+                </div>
+
+                <div className="spin">
+                  <img src="/Ellipse 10.svg" alt="" />
+                  {/* <div id="inner-spin"></div> */}
+                </div>
+
+                {/* <div id="shine"></div> */}
+              </div>
+
+
+              {/* <div id="txt"></div> */}
+              {/* </div> */}
+            </div>
+
+          </Col>
+
+          <Col sm={12}>
+            <Button className="btn-spin" onClick={e => onRandomPrize()} variant="light">Spinnnn !</Button>{' '}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+
   );
 }
